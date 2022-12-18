8c7be0f9841e20951fa1d33dcb71fa84a94b916d
Michael Viegas <michael.viegas@theprintspace.co.uk>
2022-12-18 10:57:11 +0000

    multiple courts booking attempts feature

--- src/controllers/alsakiaController.js
+++ src/controllers/alsakiaController.js
@@ -2,28 +2,62 @@ import axios from 'axios';
 import utils from '../utils.js';
 
 const clubId = 47410; // alsakia
-const courtId = 6354; // campo 2
 const hour = "19";
 const min = "00";
 
-async function relax(req, res, next) {
+const courts = [
+    6354, // campo 2
+    6353, // campo 1
+    6355 // campo 3
+];
+
+const players = {
+    180022: "Hugo Martins",
+    173264: "Darcy Mendes",
+    179439: "Mário Clara",
+    210556: "Mario Casinhas",
+    210797: "Jose Carlos Viegas",
+    118546: "Joao Ramos",
+    177693: "João Duarte",
+    200771: "Bruno Da Silva",
+    183187: "José Miguel Manhoso",
+    186340: "Luís Cristo",
+    180408: "Monia Bernardo",
+    177782: "Renato Coelho",
+    90787: "Sónia Cristina Palma",
+    177730: "Telmo Manuel Machado Pinto"
+};
+
+async function relax(req, res) {
     const token = req.query.token;
     const bookingDate = utils.getNextDayOfTheWeek("monday");
     const playerIdsCsv = "210797,180022,210556";
-    await bookCourt(res, next, token, bookingDate, playerIdsCsv);
+    await makeBooking(res, token, bookingDate, playerIdsCsv);
 }
 
-async function marisco(req, res, next) {
+async function marisco(req, res) {
     const token = req.query.token;
     const bookingDate1 = utils.getNextDayOfTheWeek("tuesday");
     const bookingDate2 = utils.getNextDayOfTheWeek("thursday");
     const bookingDate = bookingDate1 < bookingDate2 ? bookingDate1 : bookingDate2;
 
     const playerIdsCsv = "179439,180022,173264";
-    await bookCourt(res, next, token, bookingDate, playerIdsCsv);
+    await makeBooking(res, token, bookingDate, playerIdsCsv);
 }
 
-function bookCourt(res, next, token, bookingDate, playerIdsCsv) {
+async function makeBooking(res, token, bookingDate, playerIdsCsv) {
+    for (const courtId of courts) {
+        const r = await bookCourt(token, bookingDate, playerIdsCsv, courtId).catch(() => {});
+        if (!r.data?.status) continue;
+
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify(r.data));
+        return;
+    }
+    res.status(400).end("Something went wrong!");
+}
+
+function bookCourt(token, bookingDate, playerIdsCsv, courtId) {
     const method = "get";
     const url = `https://api.tiesports.com/set_a_match.asmx/save_match_v7?token=${token}&club_id=${clubId}&sport_id=2&day=${utils.formatDate(bookingDate)}&hours=${hour}:${min}&duration_minutes=90&court_id=${courtId}&gender=3&min_rating=10&max_rating=320&min_age=5&max_age=99&list_players_ids=${playerIdsCsv}&singles=false&get_players_from_friends=false&get_players_from_club=false&get_players_from_last_matches=false&get_players_from_near=false&chat_room_ids=&chat_room_request_players=false&max_players=4&go_booking=true&booking_with_lighting=false&is_public=false&promoted_match_guid=&match_title=`;
     const headers = {
@@ -32,12 +66,7 @@ function bookCourt(res, next, token, bookingDate, playerIdsCsv) {
         "accept": "application/json, text/plain, */*",
         "accept-language": "en-GB,en;q=0.9"
     };
-    return axios({ method, url, headers })
-        .then(r => {
-            const statusCode = r.data?.status ? 200 : 400;
-            res.writeHead(statusCode, { 'Content-Type': 'application/json' });
-            res.end(JSON.stringify(r.data));
-        }).catch(next);
+    return axios({ method, url, headers });
 }
 
 export default {
